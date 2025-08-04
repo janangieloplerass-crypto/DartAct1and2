09825624adb9b253e137aa4d8a32cd252d7487f4
janangieloplerass-crypto <janangieloplerass@gmail.com>
2025-08-04 17:19:47 +0800

    Activity 1 and 2 : Understanding the Dart

--- Act2.dart
+++ Act2.dart
@@ -1,8 +1,8 @@
 void main() {
   double result = calculatePerimeter(7, 0, false);
-  print('The perimeter of the rectangle with width 4 and height 0 is ' + result.toString());
+  print('The perimeter of the square is ' + result.toString());
   result = calculatePerimeter(10, 5, true);
-  print('The perimeter of the square with side 10 is ' + result.toString());
+  print('The perimeter of the rectangle is' + result.toString());
 }
 double calculatePerimeter(double width, double height, bool isSquare) {
   double perimeter;
